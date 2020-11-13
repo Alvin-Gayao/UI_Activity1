bff033019219f0f01941239e5182230b26fd35ec
Alvin_Gayao <yanah092619>
2020-11-13 21:56:44 +0800

    done? commit

--- app/src/main/java/com/example/uiactivity/HomeActivity.kt
+++ app/src/main/java/com/example/uiactivity/HomeActivity.kt
@@ -3,13 +3,50 @@ package com.example.uiactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_next.*
+import java.lang.reflect.Array.getInt
+import kotlin.random.Random
 
 class HomeActivity : AppCompatActivity() {
+    var accountImage : ImageView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        val randomInt = Random.nextInt(5) + 1
+        val drawableImages = when(randomInt) {
+            1 -> R.drawable.anonymous_mask
+            2 -> R.drawable.avatar
+            3 -> R.drawable.pennywise
+            4 -> R.drawable.predator
+            else -> R.drawable.walter_white
+        }
+          accountImageView.setImageResource(drawableImages)
+
+
+        val intent = getIntent()
+        val firstName = intent.getStringExtra("FirstName")
+        val lastName = intent.getStringExtra("LastName")
+        val email = intent.getStringExtra("Email")
+        val age = intent.getStringExtra("Age")
+        val number = intent.getStringExtra("Number")
+        val day = intent.getStringExtra("DD")
+        val month = intent.getStringExtra("MM")
+        val year = intent.getStringExtra("YY")
+
+        firstNameView.text = "First Name: "+firstName
+        lastNameView.text = "Last Name: "+lastName
+        ageView.text = age
+        emailView.text = "Email: "+email
+        birthDayView.text = "Birth Date: "+ day+"/"+month+"/"+year
+        numberView.text = "Phone Number: "+number
+
+
 
     }
 }
+
+
--- app/src/main/java/com/example/uiactivity/MainActivity.kt
+++ app/src/main/java/com/example/uiactivity/MainActivity.kt
@@ -4,21 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.CalendarView
-import android.widget.EditText
+import android.widget.ImageView
 import android.widget.SeekBar
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_next.*
+import kotlin.random.Random
+import kotlinx.android.synthetic.main.activity_main.next_layout as next_layout1
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         ageSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                ageView.text = "Age:" + progress.toString()
+                ageView.text = "Age: "+progress
             }
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
 
@@ -26,15 +27,48 @@ class MainActivity : AppCompatActivity() {
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
 
             }
-
         })
 
-        switch1.setOnCheckedChangeListener { compoundButton, onSwitch ->
+        switch1.setOnCheckedChangeListener { switch1, onSwitch ->
             if (onSwitch)
                 editNumber.visibility = View.VISIBLE
             else
                 editNumber.visibility = View.GONE
         }
+
+        nextPageBtn.setOnClickListener {
+            showNextActivity()
+        }
+
+        signUpBtn.setOnClickListener {
+            val firstName = editFirstName.text.toString()
+            val lastName = editLastName.text.toString()
+            val email = editEmail.text.toString()
+            val age = ageView.text.toString()
+            val number = editNumber.text.toString()
+            val day = datePicker.dayOfMonth.toString()
+            val month = datePicker.month.toString()
+            val year = datePicker.year.toString()
+
+            //start intent activity and pass data
+            var intent = Intent(this, HomeActivity::class.java)
+            intent.putExtra("FirstName", firstName)
+            intent.putExtra("LastName", lastName)
+            intent.putExtra("Email", email)
+            intent.putExtra("Age", age)
+            intent.putExtra("Number", number)
+            intent.putExtra("DD", day)
+            intent.putExtra("MM", month)
+            intent.putExtra("YY", year)
+
+            startActivity(intent)
+        }
+    }
+    private fun showNextActivity()
+    {
+        next_layout.visibility = View.VISIBLE
+        main_layout.visibility = View.GONE
+
     }
 
 }
